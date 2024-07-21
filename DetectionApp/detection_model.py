b7caa68e30b9155850ff72162ac859e7bea2f413
Shubham <byshubham6@gmail.com>
2024-07-21 17:53:35 +0530

    #

--- DetectionApp/detection_model.py
+++ DetectionApp/detection_model.py
@@ -3,8 +3,8 @@ import tensorflow_hub as hub
 from PIL import Image
 import numpy as np
 
-# Load the pre-trained model
-model = hub.load("https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1")
+# Load the pre-trained model using the recommended function
+model = tf.compat.v2.saved_model.load("https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1")
 
 def load_image(image_path):
     image = Image.open(image_path)
@@ -12,9 +12,36 @@ def load_image(image_path):
     image = np.array(image)
     return image
 
-def detect_objects(image_path):
+def remove_background(image_path):
     image = load_image(image_path)
-    image_tensor = tf.convert_to_tensor(image, dtype=tf.float32)[tf.newaxis, ...]
-    result = model(image_tensor)
-    return result
- 
+    input_image = tf.convert_to_tensor(image, dtype=tf.uint8)
+    input_image = tf.image.resize(input_image, (640, 640))
+    input_image = tf.expand_dims(input_image, axis=0)
+    
+    detections = model(input_image)
+    detection_boxes = detections['detection_boxes'][0].numpy()
+    detection_scores = detections['detection_scores'][0].numpy()
+    
+    # Filter detections with a threshold score
+    threshold = 0.5
+    mask = detection_scores > threshold
+    detection_boxes = detection_boxes[mask]
+    
+    # Example process: Create a mask for the detected objects
+    image_height, image_width, _ = image.shape
+    mask = np.zeros((image_height, image_width), dtype=np.uint8)
+    
+    for box in detection_boxes:
+        ymin, xmin, ymax, xmax = box
+        xmin = int(xmin * image_width)
+        xmax = int(xmax * image_width)
+        ymin = int(ymin * image_height)
+        ymax = int(ymax * image_height)
+        mask[ymin:ymax, xmin:xmax] = 1
+    
+    # Apply mask to the image to remove the background
+    result = image * np.expand_dims(mask, axis=-1)
+    result_image = Image.fromarray(result)
+    
+    return result_image
+
--- DetectionApp/urls.py
+++ DetectionApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.image_upload_view, name='image_upload'),
+    path('upload/', views.image_upload_view, name='image_upload'),
 ]
